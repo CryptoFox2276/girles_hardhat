e5600a6727190659d1ac6acc4a0753f08f66cc11
jstopcoder <jstopcoder0209@gmail.com>
2022-10-18 23:30:42 +0200

    update api key

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const API_KEY = "oDIBAyBOjmEwFFcBL35QA4rWYAfIfzIW";
+const PRIVATE_KEY = "6908da20c67046a88f437ff4f2b486c8622b95d9402a69721e376b4be93a329c";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
